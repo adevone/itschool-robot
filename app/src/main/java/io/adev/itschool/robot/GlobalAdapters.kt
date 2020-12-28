4e89bca86a28db3add0b5fecb5cbbc4fc00a64ad
kirill.terekhov <kirill.terekhov@alibaba-inc.com>
2020-12-28 09:58:08 +0300

    Add docs to GlobalAdapters

--- app/src/main/java/io/adev/itschool/robot/GlobalAdapters.kt
+++ app/src/main/java/io/adev/itschool/robot/GlobalAdapters.kt
@@ -6,18 +6,34 @@ import io.adev.itschool.robot.common.arena.Robot
 
 lateinit var globalRobot: Robot
 
+/**
+ * Передвинуться вправо на [stepsCount]
+ * Если [stepsCount] не указано, то на 1 шаг
+ */
 fun right(stepsCount: Int = 1) {
     globalRobot.right(stepsCount)
 }
 
+/**
+ * Передвинуться влево на [stepsCount]
+ * Если [stepsCount] не указано, то на 1 шаг
+ */
 fun left(stepsCount: Int = 1) {
     globalRobot.left(stepsCount)
 }
 
+/**
+ * Передвинуться вниз на [stepsCount]
+ * Если [stepsCount] не указано, то на 1 шаг
+ */
 fun down(stepsCount: Int = 1) {
     globalRobot.down(stepsCount)
 }
 
+/**
+ * Передвинуться вверх на [stepsCount]
+ * Если [stepsCount] не указано, то на 1 шаг
+ */
 fun up(stepsCount: Int = 1) {
     globalRobot.up(stepsCount)
 }
