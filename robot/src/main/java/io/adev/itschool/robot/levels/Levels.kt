f4a8d96378fed73b4a84e311f6162eb3e7085392
kirill.terekhov <kirill.terekhov@alibaba-inc.com>
2021-01-21 12:36:10 +0300

    Add homework 1

--- robot/src/main/java/io/adev/itschool/robot/levels/Levels.kt
+++ robot/src/main/java/io/adev/itschool/robot/levels/Levels.kt
@@ -21,6 +21,29 @@ pppppppppp
 """.trim()
 
 @Suppress("SpellCheckingInspection")
+val levelHomework1Denis = """
+ppppppppp
+ppp   ppp
+ppp p ppp
+p   p   p
+p ppppp p
+p     ptp
+ppp p ppp
+ppp p ppp
+ppr p  pp
+""".trim()
+
+@Suppress("SpellCheckingInspection")
+val levelHomework1Edward = """
+pppppppppppppppppp
+ppppp        ppppp
+pppp         ppppp
+p    ppppppp  pppp
+p    p     p  pppp
+ppr ppppppp tppppp
+""".trim()
+
+@Suppress("SpellCheckingInspection")
 val level3 = """
 ppppp
 pr *p
