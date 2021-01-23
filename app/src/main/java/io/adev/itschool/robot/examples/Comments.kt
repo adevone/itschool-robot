0d5f3f643b8284a31945ed64def926f052a770a7
kirill.terekhov <kirill.terekhov@alibaba-inc.com>
2021-01-23 21:23:01 +0300

    Add comments example

---
+++ app/src/main/java/io/adev/itschool/robot/examples/Comments.kt
@@ -0,0 +1,12 @@
+package io.adev.itschool.robot.examples
+
+import io.adev.itschool.robot.display
+import io.adev.itschool.robot.left
+
+// определяем функцию commentsExample
+fun commentsExample() {
+    // вызываем функцию display, передавая ей "p62" в качестве параметра password
+    display(password = "p62")
+    // вызываем функцию left, передавая ей 2 в качестве первого параметра
+    left(2)
+}
