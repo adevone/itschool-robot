82838d42ef7b630aa91238fecc93f635eba03962
Ethem74 <1437788@mail.ru>
2021-01-23 20:17:47 +0500

    Implement Eduard homework 1

---
+++ app/src/main/java/io/adev/itschool/robot/eduard/homeworks/Homework1.kt
@@ -0,0 +1,37 @@
+package io.adev.itschool.robot.eduard.homeworks
+
+import io.adev.itschool.robot.down
+import io.adev.itschool.robot.right
+import io.adev.itschool.robot.up
+
+fun eduardHomework1() {
+    lestnica()
+    hodkonem()
+    stometrovka()
+    spysk()
+    }
+
+fun lestnica() {
+    right()
+    up()
+    up()
+    right()
+}
+fun hodkonem() {
+    up()
+    right()
+}
+fun stometrovka(){
+    right()
+    right()
+    right()
+    right()
+    right()
+    right()
+    right()
+}
+fun spysk() {
+    down()
+    down()
+    down()
+}
