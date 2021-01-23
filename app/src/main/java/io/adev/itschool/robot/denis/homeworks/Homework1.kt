b2e86edd638528c511c5e3a59a4e395088bfb2a2
danceden <prugloden@yandex.com>
2021-01-23 20:12:03 +0500

    Update function denisHomework1

--- app/src/main/java/io/adev/itschool/robot/denis/homeworks/Homework1.kt
+++ app/src/main/java/io/adev/itschool/robot/denis/homeworks/Homework1.kt
@@ -1,5 +1,46 @@
 package io.adev.itschool.robot.denis.homeworks
 
+import io.adev.itschool.robot.down
+import io.adev.itschool.robot.left
+import io.adev.itschool.robot.right
+import io.adev.itschool.robot.up
+
+fun leftLowPart() {
+    right()
+    up()
+    up()
+}
+
+fun leftMiddlePart() {
+    up()
+    left()
+    left()
+    up()
+    up()
+    right()
+    right()
+}
+
+fun centralhighPart() {
+    up()
+    up()
+    right()
+    right()
+    down()
+}
+
+fun rightMiddlePart() {
+    down()
+    right()
+    right()
+    down()
+    down()
+}
+
 fun denisHomework1() {
+    leftLowPart()
+    leftMiddlePart()
+    centralhighPart()
+    rightMiddlePart()
 
 }
