425c98047c6eec7cfa096104cb5e7e42b4d08ba4
Ethem74 <1437788@mail.ru>
2021-01-31 04:11:39 +0500

    upDate my work but I broke it. I need to help

--- app/src/main/java/io/adev/itschool/robot/eduard/work/autentificate.kt
+++ app/src/main/java/io/adev/itschool/robot/eduard/work/autentificate.kt
@@ -11,23 +11,67 @@ import io.adev.itschool.robot.levels.arena6
 //p  pppp tp
 //pp   #   p
 //pppppppppp
+
+// Определяем функцию eduardAuth
 fun eduardAuth() {
+    // Вызываем функцию setArena, придавая ей значение arena6 в качестве параметра arena
     setArena(arena = arena6)
+    // Запомни, robotToken имеет значение authenticate
     val robotToken = authenticate()
+    // Вызываем функцию track
+    track()
+    // Вызываем функцию descent
+    descent()
+    // Вызываем функцию cave
+    cave()
+    // Вызываем функцию output
+    output()
+}
+// Определяем функцию track
+fun track() {
+    // Вызываем функцию moveRight, придавая ей значение 6 в качестве параметра stepsCount
     moveRight(stepsCount = 6)
+}
+// Определяем функцию descent
+fun descent() {
+    // Запомни, robotToken имеет значение authenticate
+    val robotToken = authenticate()
+    // Вызываем функцию authorize, придывая ей значение robotToken в качестве параметра token
     authorize(token = robotToken)
+    // Вызываем функцию moveDown, придавая ей значение 2 в качестве параметра stepsCount
     moveDown(stepsCount = 2)
+    // Вызываем функцию moveLeft
     moveLeft()
+    // Вызываем функцию moveDown
     moveDown()
+    // Вызываем функцию moveLeft, придавая ей значение 2 в качестве параметра stepsCount
     moveLeft(stepsCount = 2)
+}
+// Определяем функцию cave
+fun cave() {
+    // Вызываем функцию moveUp
     moveUp()
+    // Вызываем функцию moveLeft, придавая ей значение 2 в качестве параметра stepsCount
     moveLeft(stepsCount = 2)
+    // Вызываем функцию authorize, придавая ей значение robotToken в качестве параметра token
     authorize(token = robotToken)
+    // Вызываем функцию moveDown, придавая ей значение 2 в качестве параметра stepsCount
     moveDown(stepsCount = 2)
+    // Вызываем функцию moveRight
     moveRight()
+    // Вызываем функциб moveDown
     moveDown()
+}
+// Определяем функцию output
+fun output() {
+    // Запомни, robotToken имеет значение authenticate
+    val robotToken = authenticate()
+    // Вызываем функцию moveRight, придавая ей значение 2 в качестве парамметра moveRight
     moveRight(stepsCount = 2)
+    // Вызываем функцию authorize, придавая ей значение robotToken в качестве параметра token
     authorize(token = robotToken)
+    // Вызываемм функцию moveRight, придавай ей значение 4 в качестве парамметр stepsCount
     moveRight(stepsCount = 4)
+    // Вызываем функцию moveUp
     moveUp()
 }
