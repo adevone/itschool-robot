d8a140ec4ddde5a51a2111188d1537d9210be112
kirill.terekhov <kirill.terekhov@alibaba-inc.com>
2021-06-18 02:04:59 +0300

    Вып дз1

--- app/src/main/java/io/adev/itschool/robot/Global.kt
+++ app/src/main/java/io/adev/itschool/robot/Global.kt
@@ -1,14 +1,38 @@
 package io.adev.itschool.robot
 
-import io.adev.itschool.robot.galina.homework1Variant3
-import io.adev.itschool.robot.galina.passArena5
-import io.adev.itschool.robot.galina.passArena6
+import io.adev.itschool.robot.global.moveDown
+import io.adev.itschool.robot.global.moveLeft
 import io.adev.itschool.robot.global.moveRight
 import io.adev.itschool.robot.global.moveUp
-import io.adev.itschool.robot.global.setArena
-import io.adev.itschool.robot.levels.demoArena
+import io.adev.itschool.robot.global.setArena1
 
 // определяем функцию выполнить
 fun run() {
-    passArena6()
+    setArena1()
+    leftLeg()
+    leftHand()
+    head()
+    rightLeg()
+}
+
+fun leftLeg() {
+    moveRight()
+    moveUp(stepsCount = 3)
+}
+
+fun leftHand() {
+    moveLeft(stepsCount = 2)
+    moveUp(stepsCount = 2)
+    moveRight(stepsCount = 2)
+}
+
+fun head() {
+    moveUp(stepsCount = 2)
+    moveRight(stepsCount = 2)
+    moveDown(stepsCount = 2)
+}
+
+fun rightLeg() {
+    moveRight(stepsCount = 2)
+    moveDown(2)
 }
