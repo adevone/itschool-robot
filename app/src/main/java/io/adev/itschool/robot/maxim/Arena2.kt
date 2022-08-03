c0e892ec658cc4a686f8a5815cf9f5932ea0ad8b
vahit9474 <vahit94@yandex.ru>
2022-08-03 18:38:58 +0500

    usingfunctions

--- app/src/main/java/io/adev/itschool/robot/maxim/Arena2.kt
+++ app/src/main/java/io/adev/itschool/robot/maxim/Arena2.kt
@@ -1,24 +1,33 @@
 package io.adev.itschool.robot.maxim
 
+import io.adev.itschool.robot.galina.nose
+import io.adev.itschool.robot.global.display
 import io.adev.itschool.robot.global.moveDown
 import io.adev.itschool.robot.global.moveLeft
 import io.adev.itschool.robot.global.moveRight
-import io.adev.itschool.robot.global.moveUp
 import io.adev.itschool.robot.global.setArena
-import io.adev.itschool.robot.levels.arena2
+import io.adev.itschool.robot.levels.homework2Variant1Arena
 
 fun passArena2() {
-    setArena(arena = arena2)
-    moveRight(stepsCount = 6)
-    moveDown(2)
-    moveLeft()
-    moveDown()
-    moveLeft(2)
-    moveUp()
-    moveLeft(2)
-    moveDown(2)
+    setArena(arena = homework2Variant1Arena)
+    eye()
+    nose()
+    jaw()
+}
+
+private fun eye() {
+    moveRight(stepsCount = 3)
+    display(password = "p710")
+    moveDown(stepsCount = 2)
+}
+
+private fun nose() {
     moveRight()
-    moveDown()
-    moveRight(6)
-    moveUp()
+    moveDown(stepsCount = 2)
+    display(password = "p713")
+    moveLeft()
+}
+private fun jaw() {
+    moveDown(3)
+    moveLeft(2)
 }
